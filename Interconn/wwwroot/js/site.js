3c7f1695f17550650d6e0a73bc8dd78c86aa9f8c
TimothyWhite1222 <tim3269240@gmail.com>
2026-03-02 02:22:18 +0800

    增加漢堡排

--- Interconn/wwwroot/js/site.js
+++ Interconn/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.querySelector(".sidebar");
+    const mainContent = document.querySelector(".main-content");
+    const hamburger = document.getElementById("hamburger-btn");
+
+    hamburger.addEventListener("click", function () {
+        sidebar.classList.toggle("open");
+        mainContent.classList.toggle("shift");
+    });
+});
